8382fa09c33aad7fe006df3710ca59d6e36b5bbd
Thomas_Jefersson_Vaz <86377436+Thomas-Jefersson-Vaz@users.noreply.github.com>
2021-07-19 18:40:19 -0300

    Add files via upload

---
+++ Projetos Python/Calculadora Simples.py
@@ -0,0 +1,32 @@
+import time
+a=int(input("primeiro número: "))
+b=int(input("segundo número: "))
+print("Escolha o sinal")
+c=int(input("+ = 1\n"
+            "- = 2\n"
+            "× = 3\n"
+            "÷ = 4\n"))
+if c == 1:
+  d = a+b
+  print("resultado: ",d)
+  
+elif c == 2:
+  d = a-b
+  print("resultado: ",d)
+ 
+elif c == 3:
+  d = a*b
+  print("resultado: ",d)
+ 
+elif c == 4:
+  d = a/b
+  print("resultado:",d)
+  
+  
+  
+  
+  
+  
+  
+  
+  
