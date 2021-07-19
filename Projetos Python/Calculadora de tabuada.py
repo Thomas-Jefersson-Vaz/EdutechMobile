467462a9a6e0bc6020d6d0e0b5c52911a0cf5abf
Thomas_Jefersson_Vaz <86377436+Thomas-Jefersson-Vaz@users.noreply.github.com>
2021-07-19 18:46:15 -0300

    Add files via upload

---
+++ Projetos Python/Calculadora de tabuada.py
@@ -0,0 +1,23 @@
+import random
+import time
+name = input("Escreva seu nome: ")
+
+time.sleep(0.5) 
+a = random.randint(1111,9999)
+print("Gerando código")
+time.sleep(1)
+print(name,", seu código é: ",a)
+time.sleep(1)
+n = int(input("qual o número ? "))
+time.sleep(1.5)
+print("Resultado: ")
+print(n,"× 1=",n * 1)
+print(n,"× 2=",n * 2)
+print(n,"× 3=",n * 3)
+print(n,"× 4=",n * 4)
+print(n,"× 5=",n * 5)
+print(n,"× 6=",n * 6)
+print(n,"× 7=",n * 7)
+print(n,"× 8=",n * 8)
+print(n,"× 9=",n * 9)
+print(n,"× 10=",n * 10) 
