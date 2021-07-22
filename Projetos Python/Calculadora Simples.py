a172a1097cc4434835e5907454c96b37106915be
Thomas_Jefersson_Vaz <86377436+Thomas-Jefersson-Vaz@users.noreply.github.com>
2021-07-22 17:59:17 -0300

    2.0

--- Projetos Python/Calculadora Simples.py
+++ Projetos Python/Calculadora Simples.py
@@ -1,26 +1,40 @@
-import time
-a=int(input("primeiro número: "))
-b=int(input("segundo número: "))
-print("Escolha o sinal")
-c=int(input("+ = 1\n"
-            "- = 2\n"
-            "× = 3\n"
-            "÷ = 4\n"))
-if c == 1:
-  d = a+b
-  print("resultado: ",d)
+while True:
+  print("Calculadora simples")
+  line = ("*"*44)
+  print(line)
+  print("Autor: TJVG4M34R13")
+  print(line)
+  a = int(input("primeiro número: "))            
+  b = int(input("segundo número: ")) 
+  print("Escolha o sinal:")
+  c = int(input("[1] + \n"
+                "[2] - \n"
+                "[3] × \n"
+                "[4] ÷ \n\n"))
+  print("\n")              
+  if c == 1:
+    d = a+b
+    print("Resultado:",a,"+",b,"=",d,"\n","\n")       
+  elif c == 2:
+    d = a-b
+    print("Resultado:",a,"-",b,"=",d,"\n","\n") 
+  elif c == 3:
+    d = a*b
+    print("Resultado:",a,"×",b,"=",d,"\n","\n") 
+  elif c == 4:
+    d = a/b
+    print("Resultado:",a,"÷",b,"=",d,"\n","\n")
+  elif c != 1:
+    print("escolha um sinal válido\n")
+  elif c != 2:
+    print("")
+  elif c != 3:
+    print("")
+  elif c != 1:
+    print("")
+    
   
-elif c == 2:
-  d = a-b
-  print("resultado: ",d)
- 
-elif c == 3:
-  d = a*b
-  print("resultado: ",d)
- 
-elif c == 4:
-  d = a/b
-  print("resultado:",d)
+
   
   
   
@@ -28,5 +42,8 @@ elif c == 4:
   
   
   
+  
+  
+   
   
   
