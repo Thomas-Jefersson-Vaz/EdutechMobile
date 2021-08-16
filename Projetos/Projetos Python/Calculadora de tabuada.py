89fa22c5d1222203db5415a6cdf02c0aa7e06930
Thomas_Jefersson_Vaz <86377436+Thomas-Jefersson-Vaz@users.noreply.github.com>
2021-08-16 15:41:50 -0300

    Add files via upload

---
+++ Projetos/Projetos Python/Calculadora de tabuada.py
@@ -0,0 +1,13 @@
+while True:
+  n = int(input("qual o número ? \n"))
+  for i in range(0,10):
+    print(f"{i} x {n} = {i*n}")
+    
+    
+
+#while True:
+#  n = int(input("qual o número ? \n"))
+#  i = 0
+#  while i<=10:
+#    print(f"{i} x {n} = {i*n}")
+#   i+=1
